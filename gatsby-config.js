f9a50fc16756d71e20dd8e13383f85fc8db848e3
Dmitrii Shmakov <dmitrii.shmakov@revolut.com>
2020-10-28 00:55:53 +0300

    working data 2

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,8 +22,8 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
-        name: 'Shmakov blog',
-        short_name: 'Simplefolio',
+        name: 'Dmitrii Shmakov\'s website',
+        short_name: 'shmakovdima',
         start_url: '/',
         background_color: '#fff',
         theme_color: '#02aab0',

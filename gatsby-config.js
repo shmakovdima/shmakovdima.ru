c7447b65f817ec526df028205ed5c61c20a2ae38
Dmitrii Shmakov <dmitrii.shmakov@revolut.com>
2020-10-28 01:24:45 +0300

    add metrics

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,37 @@ module.exports = {
     'gatsby-plugin-sharp',
     'gatsby-plugin-offline',
     {
+      resolve: 'gatsby-plugin-yandex-metrika',
+      options: {
+        // The ID of yandex metrika.
+        trackingId: 28400231,
+        webvisor: true,
+        trackHash: true,
+        afterBody: true,
+        defer: false,
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: 'UA-59637888-1',
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+        defer: false,
+        // Any additional optional fields
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+        cookieDomain: 'shmakovdima.ru',
+      },
+    },
+    {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: 'images',
